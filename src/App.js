cfe756bc7ff26563e13eb2e1a8e41199a7f238fc
Erick Freitas <erickelsas@alunos.utfpr.edu.br>
2024-11-06 16:53:41 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SearchBooks from './pages/SearchBooks';
 import BookDetails from './pages/BookDetails';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<SearchBooks />} />
           <Route path="/book/:bookId" element={<BookDetails />} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
